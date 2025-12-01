9477965adc69ca7f2a5d3c9c7e861fc48e6a2b22
Michael Dougherty <maackle.d@gmail.com>
2025-12-01 14:44:34 -0500

    Usable crate + tests

---
+++ src/lib.rs
@@ -0,0 +1,149 @@
+//! A utility for making logs more readable by shortening and aliasing long IDs.
+
+use std::{borrow::Cow, collections::HashMap, fmt::Display, sync::LazyLock, sync::Mutex};
+
+static SHORT_ID_CACHE: LazyLock<Mutex<HashMap<String, String>>> =
+    LazyLock::new(|| Mutex::new(HashMap::new()));
+
+static ALIASES: LazyLock<Mutex<HashMap<Vec<u8>, String>>> =
+    LazyLock::new(|| Mutex::new(HashMap::new()));
+
+pub trait ShortId: Display {
+    const PREFIX: &'static str;
+    const LENGTH: usize = 4;
+
+    fn to_short_string(&self) -> String {
+        format!("{self}")
+    }
+
+    fn prefix() -> String {
+        Self::PREFIX.to_string()
+    }
+
+    fn short(&self) -> String {
+        let original = ShortId::to_short_string(self);
+        let mut s = original.clone();
+
+        s.truncate(Self::LENGTH);
+
+        let short_id = format!("{}|{s}", Self::prefix());
+        if let Some(existing) = SHORT_ID_CACHE
+            .lock()
+            .unwrap()
+            .insert(short_id.clone(), original.clone())
+        {
+            if existing != original {
+                tracing::warn!(
+                    old = ?existing,
+                    new = ?original,
+                    "short ID collision, two values have the same short ID"
+                );
+            }
+        }
+        short_id
+    }
+}
+
+pub trait AliasedId: ShortId {
+    const SHOW_SHORT_ID: bool = true;
+    const BRACKETS: (&'static str, &'static str) = ("⟪", "⟫");
+
+    fn as_bytes(&self) -> Cow<'_, [u8]>;
+
+    fn aliased(self, alias: &str) -> Self
+    where
+        Self: Sized,
+    {
+        let alias = if Self::SHOW_SHORT_ID {
+            bracketed(&format!("{}|{}", self.short(), alias), Self::BRACKETS)
+        } else {
+            bracketed(
+                &format!("{}‖{}", <Self as ShortId>::prefix(), alias),
+                Self::BRACKETS,
+            )
+        };
+        let existing = ALIASES
+            .lock()
+            .unwrap()
+            .insert(self.as_bytes().to_vec(), alias.clone());
+        if let Some(old) = existing {
+            if old != alias {
+                tracing::warn!(?old, new = ?alias, "alias already exists, replacing");
+            }
+        }
+        self
+    }
+
+    fn alias(&self) -> String {
+        ALIASES
+            .lock()
+            .unwrap()
+            .get(&*self.as_bytes())
+            .cloned()
+            .unwrap_or_else(|| self.default_alias())
+    }
+
+    fn default_alias(&self) -> String {
+        bracketed(&self.short(), Self::BRACKETS)
+    }
+}
+
+fn bracketed(s: &str, brackets: (&'static str, &'static str)) -> String {
+    format!("{}{s}{}", brackets.0, brackets.1)
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[derive(Debug, PartialEq, Eq)]
+    struct TestId(u64);
+
+    impl Display for TestId {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "{}", self.0)
+        }
+    }
+
+    impl ShortId for TestId {
+        const PREFIX: &'static str = "ID";
+    }
+
+    impl AliasedId for TestId {
+        fn as_bytes(&self) -> Cow<'_, [u8]> {
+            Cow::Owned(self.0.to_le_bytes().to_vec())
+        }
+    }
+
+    #[test]
+    fn test_short_id() {
+        tracing_subscriber::fmt::init();
+
+        let id1 = TestId(1234567890);
+        let id2 = TestId(2345678901);
+        let id3 = TestId(3456789012);
+        let idx = TestId(12349876);
+        assert_eq!(id1.short(), "ID|1234");
+        assert_eq!(id2.short(), "ID|2345");
+        assert_eq!(id3.short(), "ID|3456");
+
+        assert_eq!(idx.short(), "ID|1234");
+    }
+
+    #[test]
+    fn test_aliased_id() {
+        tracing_subscriber::fmt::init();
+
+        let id1 = TestId(1234567890).aliased("foo");
+        let id2 = TestId(2345678901).aliased("bar");
+        let id3 = TestId(3456789012).aliased("baz");
+        let idx = TestId(12349876).aliased("qux");
+
+        assert_eq!(id1.alias(), "⟪ID|1234|foo⟫");
+        assert_eq!(id2.alias(), "⟪ID|2345|bar⟫");
+        assert_eq!(id3.alias(), "⟪ID|3456|baz⟫");
+
+        assert_eq!(idx.alias(), "⟪ID|1234|qux⟫");
+    }
+}
