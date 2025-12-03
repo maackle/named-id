c3b6dbe0f58bf523cfc2e8e30e949c738a5123bc
Michael Dougherty <maackle.d@gmail.com>
2025-12-02 22:16:59 -0500

    Add empty Nameables for some primitive types

--- src/nameables/impls.rs
+++ src/nameables/impls.rs
@@ -1,5 +1,34 @@
 use super::*;
 
+#[macro_export]
+macro_rules! empty_nameables {
+    ($ty:ty) => {
+        impl Nameables for $ty {
+            fn nameables(&self) -> Vec<AnyNameable> {
+                vec![]
+            }
+        }
+    };
+}
+empty_nameables!(());
+empty_nameables!(u8);
+empty_nameables!(u16);
+empty_nameables!(u32);
+empty_nameables!(u64);
+empty_nameables!(u128);
+empty_nameables!(usize);
+empty_nameables!(i8);
+empty_nameables!(i16);
+empty_nameables!(i32);
+empty_nameables!(i64);
+empty_nameables!(i128);
+empty_nameables!(isize);
+empty_nameables!(f32);
+empty_nameables!(f64);
+empty_nameables!(bool);
+empty_nameables!(char);
+empty_nameables!(&'static str);
+
 impl<T> Nameables for Option<T>
 where
     T: Nameables,
